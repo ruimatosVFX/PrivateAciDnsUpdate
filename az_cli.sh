a2ac62af8c83fa0518e859cc1662486a4ecdfd7b
Rui Matos <128815093+ruimatosVFX@users.noreply.github.com>
2023-06-14 18:23:16 +0100

    Update az_cli.sh

--- az_cli.sh
+++ az_cli.sh
@@ -11,11 +11,11 @@ ACI_IP=$(az container show --name $ACI_INSTANCE_NAME --resource-group $RESOURCE_
 echo $ACI_IP
 
 if [ $(az network private-dns record-set a show --name $A_RECORD_NAME --resource-group $RESOURCE_GROUP --zone-name $DNS_ZONE_NAME) = false ]; then
-    echo "DNS Record does not exist. Will create it."
-    az network private-dns record-set a create --name $A_RECORD_NAME --resource-group $RESOURCE_GROUP --zone-name $DNS_ZONE_NAME --set aRecords[0].ipv4Address=$ACI_IP --ttl 60
+    echo "DNS Record does not exist. Will create it.";
+    az network private-dns record-set a create --name $A_RECORD_NAME --resource-group $RESOURCE_GROUP --zone-name $DNS_ZONE_NAME --set aRecords[0].ipv4Address=$ACI_IP --ttl 60;
 else
-    echo "DNSRecord updated"
-    az network private-dns record-set a update --name $A_RECORD_NAME --resource-group $RESOURCE_GROUP --zone-name $DNS_ZONE_NAME --set aRecords[0].ipv4Address=$ACI_IP
+    echo "DNSRecord updated";
+    az network private-dns record-set a update --name $A_RECORD_NAME --resource-group $RESOURCE_GROUP --zone-name $DNS_ZONE_NAME --set aRecords[0].ipv4Address=$ACI_IP;
 fi
 
 echo "Done"
