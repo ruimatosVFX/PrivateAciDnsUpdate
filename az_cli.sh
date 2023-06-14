4ab7947e986edc3e13d9a780fd8f39d08d7207b3
Rui Matos <128815093+ruimatosVFX@users.noreply.github.com>
2023-06-14 17:50:29 +0100

    Update az_cli.sh

--- az_cli.sh
+++ az_cli.sh
@@ -2,9 +2,14 @@
 
 az --version
 
+echo "Log in with Identity"
+
 az login --identity
 
+
 ACI_IP=$(az container show --name $ACI_INSTANCE_NAME --resource-group $RESOURCE_GROUP --query ipAddress.ip --output tsv)
+
+echo $ACI_IP
 
 az network private-dns record-set a update --name $A_RECORD_NAME --resource-group $RESOURCE_GROUP --zone-name $DNS_ZONE_NAME --set aRecords[0].ipv4Address=$ACI_IP
 
